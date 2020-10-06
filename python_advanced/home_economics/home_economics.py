e757b076c51a906deac73e47a92ad91da58e8b92
Daniel Cohen Hillel <danielcohenhillel@gmail.com>
2020-10-06 19:34:33 +0300

    Solved 6. Home Economics - python advanced

--- python_advanced/home_economics/home_economics.py
+++ python_advanced/home_economics/home_economics.py
@@ -6,15 +6,21 @@ Write a function that calculate how many month one should save money to buy new 
 
 
 def home_economics(startPriceOld, startPriceNew, savingPerMonth, percentLossByMonth):
-    # write your code here
-    pass
+    i = 0
+    diff = startPriceOld - startPriceNew
+    loss = 1 - percentLossByMonth / 100
+    while (diff + i * savingPerMonth) < 0:
+        diff *= loss
+        loss -= 0.005 if i % 2 == 0 else 0
+        i += 1
+    return (i, round(diff + savingPerMonth * i))
 
 
 # small test to check it's work.
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    ret = home_economics(2000, 8000, 1000, 1.5)
+    ret = home_economics(2100, 8000, 1000, 1.5)
     if ret[0] == 6 and ret[1] == 766:
-        print("HAZA! simple test pass")
+        print("\33[1m\33[32mHAZA!\33[0m simple test pass :)")
     else:
-        print("Oops, it's not working yet")
+        print("\33[1m\33[31mOops\33[0m, it's not working yet :(")
