9fd5e0b18e3138eb9557f7ac92dd10b0c5927f0a
Daniel Cohen Hillel <danielcohenhillel@gmail.com>
2020-10-09 15:31:58 +0300

    Solved python_advanced 7. Ejected

--- python_advanced/ejected/ejected.py
+++ python_advanced/ejected/ejected.py
@@ -1,5 +1,5 @@
 """
-Black Biss - Advance Python
+Black Biss - Advance Python, Author: Daniel Cohen Hillel, ID: 212553804
 
 Write a function that calculate how far is our ejected pilot.
 Run this program and insert the following lines (each end with Enter):
@@ -12,13 +12,34 @@ RIGHT 2
 
 and the result should be 2
 """
+import traceback
+
+move = {"right": 1, "left": -1, "up": 1j, "down": -1j}
 
 
 def ejected():
-    # write your code here
-    pass
+    """Calculate how far is our ejected pilot.
+    Input to stdin UP/DOWN/RIGH followd by a space and the number of steps in that
+    direction     repeatedly, when you want to eject, enter '0' and off you go ;)"""
+
+    position = 0 + 0j
+    while True:
+        inp = input()
+        if inp == "0":
+            break
+        try:
+            direction, steps = inp.lower().split(" ")
+            position += move[direction] * float(steps)
+        except ValueError:
+            traceback.print_exc()
+            print('\33[1m\33[31mInput must be of the form:\n>    \33[33m`direction float`\33[0m')
+        except KeyError:
+            traceback.print_exc()
+            print('\33[1m\33[33m'+direction, '\33[0mis not a valid direction!')
+
+    return round(abs(position))
 
 
 # small test to check it's work.
-if __name__ == '__main__':
-    print(ejected())
+if __name__ == "__main__":
+    print("The pilot is vibing\33[1m\33[34m", ejected(), "units\33[0m away from the origin🌴")
