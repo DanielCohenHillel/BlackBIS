d78f9362547ace081132e772c3cae9667551a01e
Daniel Cohen Hillel <danielcohenhillel@gmail.com>
2020-10-09 16:04:21 +0300

    Solved python_advanced 8. Pirates of the Biss

--- python_advanced/pirates_of_the_biss/dejumble.py
+++ python_advanced/pirates_of_the_biss/dejumble.py
@@ -6,13 +6,10 @@ Write a function that suggest which word is the Pirates meant to write.
 
 
 def dejumble(word, potentional_words):
-    # write your code here
-    pass
-
+    return [w for w in potentional_words if sorted(w.lower())==sorted(word.lower())]
 
 # small test to check it's work.
 if __name__ == '__main__':
-
     ret = dejumble("orspt", ["sport", "parrot", "ports", "matey"])
     if len(ret) == 2 and set(ret) == set(["sport", "ports"]):
         print("HAZA! simple test pass")
